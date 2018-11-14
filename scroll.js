262b1fefea567809805f1f017092dce0550d165a
JamesRobb03 <j.a.m.robb@dundee.ac.uk>
2018-11-14 15:49:05 +0000

    Added scrolling button. Added About me text. Added margin to skills.

---
+++ scroll.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+//This is not my own work. this was taken from a tutorial site on how to create
+//a scrolling button. it can be found here: https://www.solodev.com/blog/web-design/adding-a-scroll-down-anchor-to-your-website.stml
